88381d2316b1e4b597676820364266ba2b15b5bb
Your Name <ansokwin@gmail.com>
2025-11-20 22:36:45 +0700

    sdfdsf

--- next.config.mjs
+++ next.config.mjs
@@ -18,18 +18,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'picsum.photos',
       },
-
-      // Local networks
-      { protocol: 'http', hostname: '192.168.1.7', port: '8000' },
-      { protocol: 'http', hostname: '192.168.1.9', port: '8000' },
-      { protocol: 'http', hostname: '192.168.1.8', port: '8000' },
-      { protocol: 'http', hostname: '192.168.1.5', port: '8000' },
-      { protocol: 'http', hostname: '192.168.1.2', port: '8000' },
-      { protocol: 'http', hostname: '192.168.1.3', port: '8000' },
-      { protocol: 'http', hostname: '192.168.1.4', port: '8000' },
-      { protocol: 'http', hostname: '192.168.0.120', port: '8000' },
-
-      { protocol: 'http', hostname: 'example.com' },
+      {
+        protocol: 'http',
+        hostname: 'example.com',
+      },
     ],
   },
 };
